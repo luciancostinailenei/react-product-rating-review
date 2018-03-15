0e32d93a6c676190d84f65e1f03de530c6cd0a5d
Lucian Ailenei <l.ailenei@levi9.com>
2018-03-15 11:59:27 +0200

    Added product details widgets visual mocks

--- src/modules/Product/Product.js
+++ src/modules/Product/Product.js
@@ -31,6 +31,56 @@ class Product extends Component {
           <h2 className="Product__title">Product title TST42chk</h2>
 
           <p className="Product__price">$193</p>
+
+          <p className="Product__specifications-list">
+            <ul className="list list--no-padding-left list--squared Product__specification">
+              <li>Lorem ipsum dolor sit amet, consectetur </li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing </li>
+              <li>Lorem ipsum dolor</li>
+              <li>Lorem ipsum dolor sit amet</li>
+            </ul>
+          </p>
+
+          <div className="Product__color-picker color-picker">
+            <p className="color-picker__title">
+              Color:
+              <span className="color-picker__selected-color color-picker__selected-color--dark-blue">
+                DARK BLUE
+              </span>
+            </p>
+            <span className="color-picker__color color-picker__color--dark-blue color-picker__color--selected">
+              DB
+            </span>
+            <span className="color-picker__color color-picker__color--blue">
+              B
+            </span>
+            <span className="color-picker__color color-picker__color--gray">
+              Y
+            </span>
+            <span className="color-picker__color color-picker__color--black">
+              R
+            </span>
+          </div>
+
+          <div className="Product__size-picker size-picker">
+            <span className="size-picker__title">Size: </span>
+
+            <span className="size-picker__size size-picker__size--out-of-stock">
+              37
+            </span>
+            <span className="size-picker__size size-picker__size--selected">
+              38
+            </span>
+            <span className="size-picker__size">39</span>
+            <span className="size-picker__size">40</span>
+            <span className="size-picker__size">41</span>
+            <span className="size-picker__size">42</span>
+          </div>
+
+          <button className="Product__add-to-cart btn btn--catchy">
+            <i class="fas fa-cart-arrow-down" /> ADD TO CART
+          </button>
         </div>
 
         <div className="Product__product-reviews">
